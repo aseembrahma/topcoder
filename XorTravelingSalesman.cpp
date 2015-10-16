a82ecb69df3b072088a66c966e3b9492c16a9e0d
Aseem Brahma <abrahma@qmt.qualcomm.com>
2015-10-16 15:57:28 -0700

    XorTravelingSalesman

--- XorTravelingSalesman.cpp
+++ XorTravelingSalesman.cpp
@@ -13,15 +13,17 @@
 #include <numeric>
 #include <bitset>
 #include <stack>
+#include <utility>
 
 #define DEBUG 0
 #define ARR_SIZE(x) ((sizeof(x)) / (sizeof(x[0])))
 #define INF_INT numeric_limits<int>::max()
 
+using namespace std;
+
 typedef unsigned long long ULL;
 typedef long long LL;
-
-using namespace std;
+typedef pair<int, int> pii;
 
 vector<string> &split(const string &s, char delim, vector<string> &elems) {
     stringstream ss(s);
@@ -59,27 +61,38 @@ void print_vector(const vector<T> &v) {
 }
 
 class XorTravelingSalesman {
+  /* Assume that the graph contains nodes of the form pair(node_id, value)
+     where 'value' is the value of the node when it is entered (and therefore
+     includes the XOR along the way). A simple search on this graph (since
+     the search space is guaranteed to not be too large) should return the
+     solution */
+
 public:
   int maxProfit(vector<int> cityValues, vector<string> roads) {
-    vector<bool> g_visited(cityValues.size(), false);
-    // 1-indexed
-    vector<int> max_bit_set(cityValues.size(), 0);
-    for (int i = 0; i < cityValues.size(); ++i) {
-      int j = 0;
-      for (int temp = cityValues[i]; temp; ++j, temp = temp >> 1);
-      max_bit_set[i] = j;
-      cout << j << endl;
+    map<pii, bool> visited;
+    queue<pii> q;
+    pii start = make_pair(0, cityValues[0]);
+    int max_profit = numeric_limits<int>::min();
+
+    visited[start] = true;
+    q.push(start);
+
+    while (!q.empty()) {
+      pii u = q.front();
+      q.pop();
+      max_profit = max(max_profit, u.second);
+
+      for (int i = 0; i < roads[u.first].size(); ++i) {
+        if (roads[u.first][i] == 'Y') {
+          pii v = make_pair(i, u.second ^ cityValues[i]);
+          if (!visited[v]) {
+            visited[v] = true;
+            q.push(v);
+          }
+        }
+      }
     }
 
-    queue<int> gq;
-    g_visited[0] = true;
-    gq.push(0);
-
-    while (!gq.empty()) {
-
-    }
-
-    int max_profit = 0;
     return max_profit;
   }
 
@@ -112,6 +125,78 @@ int main(int argc, char* argv[]) {
                      "YNNNN",
                      "YNNNN"},
       14);
+  }
+  {
+    test.runTest(
+      vector<int>{556},
+      vector<string>{"N"},
+      556);
+  }
+  {
+    test.runTest(
+      vector<int>{0, 4, 8, 32, 512},
+      vector<string>{"NYYYY",
+                     "YNNNN",
+                     "YNNNN",
+                     "YNNNN",
+                     "YNNNN"},
+      556);
+  }
+  {
+    test.runTest(
+      vector<int>{37, 1, 19, 64, 42, 41, 64, 64, 54, 16, 256, 36, 64, 2, 4, 2, 62, 29, 58, 64, 1, 32, 16,
+ 256, 17, 2, 17, 4, 1, 64, 21, 8, 256, 63, 3, 1, 43, 15, 8, 39, 41, 8, 16, 8, 16, 256, 64, 512, 45, 64},
+      vector<string>{"NNNNNNYYYYNNNNNNNNNNNNNNNNNNNNNNNNNYNNNNNNNNNNNNNN",
+ "NNNNNNNNNNNNNNNNYNNNNNNNYNNNNNNNNNNNNNNNNYYNNNYYNN",
+ "NNNNNYYNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN",
+ "NNNNNNNYNNNNNNNNNNYNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN",
+ "NNNNNNNNNNNNNNNYNNYNYNNNNNNYNNNNNNNNNNYNNNNNNNNNNN",
+ "NNYNNNYNNNNNNNNYNNYNNNYYNNNYNYNNNNYNNNNNNNNYNNNNNN",
+ "YNYNNYNYNNNNNNNYNNNNNNNNNNNNNNNNNNNYNNNNNNNNYNNYNN",
+ "YNNYNNYNYNYYNNNNNNNNNNNNNNNNNNNNNNYNNYNNNNNNNNNNNN",
+ "YNNNNNNYNNNNNNNNNNNNNNYNYNNNNNNNNNNYYYNNNNNNNYNNNY",
+ "YNNNNNNNNNNNNNNNNYNYNYNYYNNNYNNNNYNNNNNNNNNNNNNNNY",
+ "NNNNNNNYNNNNYNNNNNNNNYYNNNYYNNNNYNYYNNNNNNNNNNNNNN",
+ "NNNNNNNYNNNNNNYNNNNYYNNNYNNYYNNNNNNNNNNNNNYNYNNNNN",
+ "NNNNNNNNNNYNNNNNYNNNNYNNNNNNNNNNYNYNNYNYNNNYNYNNNN",
+ "NNNNNNNNNNNNNNNYNNNNNNNNNYNNNNNNNNNNNNYNNNNNNNNYNN",
+ "NNNNNNNNNNNYNNNNNYNYNNYYNNNNNYNNNNNNNNNYNNYNNYNNNN",
+ "NNNNYYYNNNNNNYNNNYYNNYNNNYNYYNNNNNNNNNYYYNNYNNYNYN",
+ "NYNNNNNNNNNNYNNNNNNNYNNNYYNNNYNNNNYNNNNNNNNNNNNNNN",
+ "NNNNNNNNNYNNNNYYNNNNNNYNNNYNNNNNYNNYNYYNNNNYNNNYNN",
+ "NNNYYYNNNNNNNNNYNNNNNYNYNYNNNNNNNNYNNNNNNNNNNNNNNN",
+ "NNNNNNNNNYNYNNYNNNNNNYNYYYNNNNNNNNNNNYNNYNNNNNYNNN",
+ "NNNNYNNNNNNYNNNNYNNNNYNNNYYNNNYNNNYNNNNNNNNNNYNYNY",
+ "NNNNNNNNNYYNYNNYNNYYYNYNNNNNNNNYNYNNNNNNNNNNYNNNNN",
+ "NNNNNYNNYNYNNNYNNYNNNYNNNNNNNNNNNYNNYNYNNYNNNNNNNN",
+ "NNNNNYNNNYNNNNYNNNYYNNNNNNNNNNNNNNNNNNNNNNYNNNYNNN",
+ "NYNNNNNNYYNYNNNNYNNYNNNNNNNNNNYNNNNNNYNNNYNNYNNNNN",
+ "NNNNNNNNNNNNNYNYYNYYYNNNNNNYNNNNNNNNNNNYYNNNNNNNYN",
+ "NNNNNNNNNNYNNNNNNYNNYNNNNNNNNYNNNNYNNNNNNYYNNNNYNN",
+ "NNNNYYNNNNYYNNNYNNNNNNNNNYNNNYYNYNNNNNNNNNNNNNNNNN",
+ "NNNNNNNNNYNYNNNYNNNNNNNNNNNNNYNNNNYNNNNNNNNYNNYNYN",
+ "NNNNNYNNNNNNNNYNYNNNNNNNNNYYYNNNNNNNNYNNNNYNNNNNNN",
+ "NNNNNNNNNNNNNNNNNNNNYNNNYNNYNNNNNYNNNNNNNNNNNNNNNY",
+ "NNNNNNNNNNNNNNNNNNNNNYNNNNNNNNNNYNNNNNNNNNYNNNNNNN",
+ "NNNNNNNNNNYNYNNNNYNNNNNNNNNYNNNYNNNYYNNNNNYNNNYNNN",
+ "NNNNNNNNNYNNNNNNNNNNNYYNNNNNNNYNNNNNNNYNNYNNNNNNNN",
+ "NNNNNYNYNNYNYNNNYNYNYNNNNNYNYNNNNNNYYNYNYNYNNNNNYN",
+ "YNNNNNYNYNYNNNNNNYNNNNNNNNNNNNNNYNYNNNNNYNNYNNNYNN",
+ "NNNNNNNNYNNNNNNNNNNNNNYNNNNNNNNNYNYNNNNNNYNNNNNNYN",
+ "NNNNNNNYYNNNYNNNNYNYNNNNYNNNNYNNNNNNNNNNNYNNNNYNNN",
+ "NNNNYNNNNNNNNYNYNYNNNNYNNNNNNNNNNYYNNNNYNNNNNNNNNY",
+ "NNNNNNNNNNNNYNYYNNNNNNNNNYNNNNNNNNNNNNYNNNNNYNYYNN",
+ "NNNNNNNNNNNNNNNYNNNYNNNNNYNNNNNNNNYYNNNNNNNNNNNNNN",
+ "NYNNNNNNNNNNNNNNNNNNNNYNYNYNNNNNNYNNYYNNNNNNNNNNNN",
+ "NYNNNNNNNNNYNNYNNNNNNNNYNNYNNYNYYNYNNNNNNNNNYNNNNN",
+ "NNNNNYNNNNNNYNNYNYNNNNNNNNNNYNNNNNNYNNNNNNNNNNNNNY",
+ "NNNNNNYNNNNYNNNNNNNNNYNNYNNNNNNNNNNNNNNYNNYNNYNNNY",
+ "NNNNNNNNYNNNYNYNNNNNYNNNNNNNNNNNNNNNNNNNNNNNYNNNNN",
+ "NYNNNNNNNNNNNNNYNNNYNNNYNNNNYNNNYNNNNYNYNNNNNNNNNN",
+ "NYNNNNYNNNNNNYNNNYNNYNNNNNYNNNNNNNNYNNNYNNNNNNNNNN",
+ "NNNNNNNNNNNNNNNYNNNNNNNNNYNNYNNNNNYNYNNNNNNNNNNNNN",
+ "NNNNNNNNYYNNNNNNNNNNYNNNNNNNNNYNNNNNNNYNNNNYYNNNNN"},
+      895);
   }
 }
 
